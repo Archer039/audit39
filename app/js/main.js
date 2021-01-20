f706dfea0ab77d34450f1def2ef649a4184303ca
Kuzmin Roman <archer039@gmail.com>
2021-01-20 14:04:32 +0200

    add modal window and script

--- app/js/main.js
+++ app/js/main.js
@@ -2,3 +2,11 @@ $(".menu-icon").on("click", function(){
     $(".menu-item").toggle("fast")
 })
 
+// модальное окно "заказ услуги"
+$(".order").on("click", function(){
+    $(".order-modal").css("display", "flex")
+})
+
+$(".close").on("click", function(){
+    $(".order-modal").css("display", "none")
+})
